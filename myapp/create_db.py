391f1c227b12d62022f9691c1ca2f9c08418c874
kisaki <bykisaki@gmail.com>
2025-05-02 20:17:38 +0900

    init db

--- myapp/create_db.py
+++ myapp/create_db.py
@@ -1,22 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect("data.db")
+conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
 c.execute('''
 CREATE TABLE IF NOT EXISTS posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user TEXT NOT NULL,
-    content TEXT,
-    image TEXT
+    username TEXT NOT NULL,
+    message TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
 )
 ''')
 
 conn.commit()
-conn.close()
-
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-for row in c.execute('SELECT * FROM posts'):
-    print(row)
 conn.close()
