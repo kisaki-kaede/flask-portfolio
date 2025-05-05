eb19a69c74e0db45c60b14abd5037035fe0b2977
kisaki <bykisaki@gmail.com>
2025-05-05 12:02:23 +0900

    fix: add sqlite3 import

--- myapp/app.py
+++ myapp/app.py
@@ -5,6 +5,8 @@ import os
 
 from werkzeug.utils import secure_filename
 
+import sqlite3
+
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'data.db')
